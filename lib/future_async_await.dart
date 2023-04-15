bc29e8b8170128e59fe209bdb309dc2cbad13eef
frdnhmrf <frdnhmrf@gmail.com>
2023-04-15 15:20:35 +0700

    Penambahan await dalam function getFirstname dan getLastName

--- lib/future_async_await.dart
+++ lib/future_async_await.dart
@@ -12,8 +12,8 @@ Future<String> sayHello(String name) async {
 
 Future<void> main(List<String> args) async {
   try {
-    var firstName = getFirstName();
-    var lastName = getLastName();
+    var firstName = await getFirstName();
+    var lastName = await getLastName();
     var hello = await sayHello("$firstName $lastName");
     print(hello);
   } catch (e) {
