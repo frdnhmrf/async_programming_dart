f821428a764e519f961542dedf79bf4c56fa8a29
frdnhmrf <frdnhmrf@gmail.com>
2023-04-15 12:12:11 +0700

    initial commit

---
+++ bin/belajar_async_programming.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_async_programming/belajar_async_programming.dart' as belajar_async_programming;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_async_programming.calculate()}!');
+}
---
+++ test/belajar_async_programming_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_async_programming/belajar_async_programming.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
